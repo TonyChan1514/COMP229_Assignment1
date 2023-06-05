f631806dd11241ef91952ed45fb9d118ca27829e
TonyChan1514 <tonychan1514@gmail.com>
2023-06-04 21:24:27 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,15 +6,17 @@
 
 const express = require('express');
 const path = require('path');
-const logger = require('morgan');
 const router = require('./routes/index');
 const app = express();
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// moddlewares
+// Set the static path
+app.use(express.static('public'));
+
+// Router
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/', router);
@@ -23,8 +25,5 @@ app.use('/', router);
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server started on port 3000');
 });
-
-// Set the static path
-app.use(express.static('public'));
 
 module.exports = app;
