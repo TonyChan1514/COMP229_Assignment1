f1b9b89cebf59a6afc7107b5ad7fa8deddc9b6a9
TonyChan1514 <tonychan1514@gmail.com>
2023-06-04 20:07:01 -0400

    Update Port

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/', router);
 
 // Start the server
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server started on port 3000');
 });
 
